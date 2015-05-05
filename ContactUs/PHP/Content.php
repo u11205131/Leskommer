e156cb8b98fd5b74e820677c47e5d544f7d9ba86
Dail Jonker <dailjonker@gmail.com>
2015-05-05 23:15:23 +0200

    Email and User Data
    User data is saved in a unique text file containing a timestamp in the
    name.
    
    Email is not yet working.

--- ContactUs/PHP/Content.php
+++ ContactUs/PHP/Content.php
@@ -1,5 +1,6 @@
 <div class="content">
 	<h1>Contact Us</h1><br>
+        <form action="" method="post">
 	<table class="contentTable">
 		<tr>
 			<td class="leftColumn">
@@ -15,8 +16,8 @@
 
 					<input placeholder="Name" type="text" id="name" name="name">
 					<input placeholder="Email" type="text" id="email" name="email"><br/><br/>
-					
-					<textarea rows="4" cols="50" placeholder="Question" name="comment" form="usrform"></textarea><br/><br/>
+					<!--form="usrform"-->
+					<textarea type="text"  placeholder="Question" name="question" id="question" rows="4" cols="50" ></textarea><br/><br/>
 
 					<input id="submit" name="submit" value="Send" type="submit"/>
 
@@ -36,5 +37,33 @@
 			</td>
 		</tr>
 	</table>
+        </form>
 
 </div>
+
+
+<?php 
+    if(isset($_POST['submit']))
+    {
+        $to = "leskommersolutions@gmail.com"; // this is your Email address
+        $from = $_POST['email']; // this is the sender's Email address
+        $name = $_POST['name'];
+        $subject = "User question";
+        $question = $_POST['question'];
+        $message = $name . " wrote the following:" . "\n\n" . $question;
+        $message2 = "Here is a copy of your message " . $name . "\n\n" . $question;
+        $time = date("h-i-sa");
+        
+        //Create textfile that stores user data
+        $myfile = fopen("../Correspondence/".$name."User".$time.".txt", "w") or die("Unable to open file!");
+        $txt = "Name: ".$name."\n Email: ".$from."\n Question: ".$question;
+        fwrite($myfile, $txt);
+        fclose($myfile);
+
+        //Sending the email PROBLEM
+        $headers = "From: $from";
+        $headers2 = "From: $to";
+        mail($to,$subject,$message, $headers);
+        mail($from,$subject,$message2, $headers2); // sends a copy of the message to the sender
+     }
+?>
